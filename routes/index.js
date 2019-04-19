d6d9437c37cf903be0c7b065dec394be666b5d70
Fabio Morbec <morbecfm@gmail.com>
2019-04-19 16:33:20 +0200

    Add to-do-list.txt and nodebrainz package

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const NB = require('nodebrainz');
+const nb = new NB({ userAgent: 'my-awesome-app/0.0.1 ( http://localhost:3000 )' });
 
 /* GET home page */
 router.get('/', (req, res, next) => {
+  nb.search('artist', { artist: 'tool', country: 'US' }, function(err, response) {
+    console.log(response);
+  });
   res.render('index');
 });
 
