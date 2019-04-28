484d948409f8fd6c916ca5c0cf1af410bcaf0a22
Fabio Morbec <morbecfm@gmail.com>
2019-04-28 16:24:31 +0200

    Follow/unfollow artist when visiting the Artist details page

--- routes/artist/profile.js
+++ routes/artist/profile.js
@@ -2,66 +2,70 @@ require('dotenv').config();
 
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/user');
 const BITAPIHandler = require('../../api/BITAPIHandler');
 const MBAPIHandler = require('../../api/MBAPIHandler');
 const SpotifyWebApi = require('spotify-web-api-node');
 const axios = require('axios');
 
 const spotifyApi = new SpotifyWebApi({
-    clientId: `${process.env.SPOTIFY_CLIENT_ID}`,
-    clientSecret: `${process.env.SPOTIFY_CLIENT_SECRET}`,
-    redirectUri: 'http://localhost:3000/'
+  clientId: `${process.env.SPOTIFY_CLIENT_ID}`,
+  clientSecret: `${process.env.SPOTIFY_CLIENT_SECRET}`,
+  redirectUri: 'http://localhost:3000/'
 });
 
 spotifyApi
-    .clientCredentialsGrant()
-    .then(data => {
-        spotifyApi.setAccessToken(data.body['access_token']);
-    })
-    .catch(error => {
-        console.error('Something went wrong when retrieving an access token', error);
-    });
+  .clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.error('Something went wrong when retrieving an access token', error);
+  });
 
 const bandsInTown = new BITAPIHandler(`${process.env.BASE_URL}`, `${process.env.BANDS_IN_TOWN_APP_ID}`);
 const musicBrainz = new MBAPIHandler(`${process.env.MB_BASE_URL}`);
 
 router.get('/:artistId', async (req, res, next) => {
-    const { artistId } = req.params;
-    const artistData = await musicBrainz.getArtistInfoById(artistId);
-    const artistInfo = await bandsInTown.getArtistInfo(artistData.name);
+  const { artistId } = req.params;
+  const artistData = await musicBrainz.getArtistInfoById(artistId);
+  const artistInfo = await bandsInTown.getArtistInfo(artistData.name);
 
-    const lastFMQuery = `https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&mbid=${artistId}&api_key=f4529f53125ed399f4bee0c8d07d088b&format=json`;
-    const lastFMReq = await axios.get(lastFMQuery);
-    const lastFMInfo = lastFMReq.data
+  const lastFMQuery = `https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&mbid=${artistId}&api_key=f4529f53125ed399f4bee0c8d07d088b&format=json`;
+  const lastFMReq = await axios.get(lastFMQuery);
+  const lastFMInfo = lastFMReq.data;
 
-    const { artist } = lastFMInfo;
-    artist.name = artist.name.toLowerCase();
+  const { artist } = lastFMInfo;
+  artist.name = artist.name.toLowerCase();
 
-    const { summary } = artist.bio;
-    const bioIndx = summary.indexOf('<');
-    artist.bio.summary = summary.substr(0, bioIndx);
+  const { summary } = artist.bio;
+  const bioIndx = summary.indexOf('<');
+  artist.bio.summary = summary.substr(0, bioIndx);
 
-    artist.tags.tag.forEach(tag => tag.name = tag.name.toLowerCase());
-    artist.similar.artist.forEach(artist => artist.name = artist.name.toLowerCase());
+  artist.tags.tag.forEach(tag => (tag.name = tag.name.toLowerCase()));
+  artist.similar.artist.forEach(artist => (artist.name = artist.name.toLowerCase()));
 
-    const isAuthenticated = req.isAuthenticated();
+  let following = false;
+  const isAuthenticated = req.isAuthenticated();
+  if (isAuthenticated) {
+    const userdb = await User.findById(req.user._id);
+    following = userdb.artists.find(artist => artist.id === artistInfo.id);
+  }
 
-    
-    res.render('artist/profile.hbs', { artistData, artistInfo, artist, isAuthenticated});
+  res.render('artist/profile.hbs', { artistData, artistInfo, artist, isAuthenticated, following });
 });
 
 router.get('/:artistId/spot', (req, res, next) => {
-    const { artistId } = req.params;
-
-    spotifyApi
-        .getArtist(`${artistId}`)
-        .then(data => {
-            const { body } = data;
-            res.render('artist/spotProfile.hbs', body);
-        })
-        .catch(err => {
-            console.error(`Error when looking for artist`, err);
-        });
+  const { artistId } = req.params;
+  spotifyApi
+    .getArtist(`${artistId}`)
+    .then(data => {
+      const { body } = data;
+      res.render('artist/spotProfile.hbs', body);
+    })
+    .catch(err => {
+      console.error(`Error when looking for artist`, err);
+    });
 });
 
 module.exports = router;
